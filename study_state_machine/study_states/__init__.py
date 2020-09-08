65b7b747ccb4ba1a0bf34c8f83967f1e15291b6f
Cyril Lopez <cyrlop06@hotmail.com>
2020-09-08 11:06:13 +0200

    Removed expression states

--- study_state_machine/study_states/__init__.py
+++ study_state_machine/study_states/__init__.py
@@ -4,8 +4,6 @@ import inspect
 from study_state_machine.study_states.rna_seq import RNASeqState, BiokitUploadState
 from study_state_machine.study_states.generic import GenericState, RegisteredState, \
     DatasetState, ProcessedState, AnalyzedState, LockedState
-from study_state_machine.study_states.expression import ExpressionRegisteredState, \
-     ExpressionDatasetState, ExpressionLockedState
 
 """
 Store all above imported states in dict <name, class>
@@ -18,4 +16,3 @@ for name, obj in inspect.getmembers(sys.modules[__name__], inspect.isclass):
 __all__ = ["RNASeqState", "BiokitUploadState", "available_states"]
 __all__ += ["GenericState", "RegisteredState", "DatasetState", "ProcessedState",
     "AnalyzedState", "LockedState"]
-__all__ += ["ExpressionRegisteredState", "ExpressionDatasetState", "ExpressionLockedState"]
