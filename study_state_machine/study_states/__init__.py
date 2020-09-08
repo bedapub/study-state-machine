1cbd81cf86a5cdad48e3c42259f879905ba7caee
Cyril Lopez <cyrlop06@hotmail.com>
2020-09-08 09:57:12 +0200

    Added more generic states

--- study_state_machine/study_states/__init__.py
+++ study_state_machine/study_states/__init__.py
@@ -2,7 +2,8 @@ import sys
 import inspect
 
 from study_state_machine.study_states.rna_seq import RNASeqState, BiokitUploadState
-from study_state_machine.study_states.generic import GenericState
+from study_state_machine.study_states.generic import GenericState, RegisteredState, \
+    DatasetState, ProcessedState, AnalyzedState, LockedState
 from study_state_machine.study_states.expression import ExpressionRegisteredState, \
      ExpressionDatasetState, ExpressionLockedState
 
@@ -14,5 +15,7 @@ available_states = {}
 for name, obj in inspect.getmembers(sys.modules[__name__], inspect.isclass):
     available_states[name] = obj
 
-__all__ = ["RNASeqState", "BiokitUploadState", "available_states", "GenericState",
-    "ExpressionRegisteredState", "ExpressionDatasetState", "ExpressionLockedState"]
+__all__ = ["RNASeqState", "BiokitUploadState", "available_states"]
+__all__ += ["GenericState", "RegisteredState", "DatasetState", "ProcessedState",
+    "AnalyzedState", "LockedState"]
+__all__ += ["ExpressionRegisteredState", "ExpressionDatasetState", "ExpressionLockedState"]
