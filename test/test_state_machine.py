ce401e3a4556351820e1c8ec77a6d58e8ff8ef1b
Cyril Lopez <cyrlop06@hotmail.com>
2021-08-05 14:24:03 +0200

    Migrated states to database and refactored states processing

--- test/test_state_machine.py
+++ test/test_state_machine.py
@@ -1,47 +1,43 @@
 import unittest
 
 from study_state_machine.interfaces import IStudyState
-from study_state_machine.errors import StateNotFoundException, BehaviorNotAllowedException
-from study_state_machine.study_states import RegisteredState, DatasetState
+from study_state_machine.errors import (
+    StateNotFoundException,
+    BehaviorNotAllowedException,
+)
 from study_state_machine.context import Context
 
 
 class ContextTestCase(unittest.TestCase):
 
+    available_states = [{"name": "RegisteredState"}, {"name": "DatasetState"}]
+
     def test_none_initial_state(self):
-        context = Context()
+        context = Context(self.available_states)
         self.assertIsNone(context.current_state)
 
     def test_set_current_state(self):
-        context = Context()
-        context.current_state = RegisteredState()
-        self.assertEqual(context.current_state, RegisteredState(context))
+        context = Context(self.available_states)
+        context.current_state = "RegisteredState"
+        self.assertEqual(context.current_state, "RegisteredState")
 
     def test_load_unknown_state(self):
-        context = Context()
+        context = Context(self.available_states)
         with self.assertRaises(StateNotFoundException):
             context.load_state("unknown")
 
     def test_same_state_w_different_context_not_equal(self):
-        context = Context(RegisteredState())
-        self.assertNotEqual(context.current_state, DatasetState())
+        context = Context(self.available_states, "RegisteredState")
+        self.assertNotEqual(context.current_state, "DatasetState")
 
     def test_get_available_states(self):
-        context = Context()
-        name, cls = context.available_states
+        context = Context(self.available_states)
+        state_names, states = context.available_states
 
-        self.assertEqual(len(name), 2)
-
-    def test_behavior_create_study(self):
-        context = Context(IStudyState)
-
-        for func in [context.create_study, context.change_state, context.add_sample]:
-            with self.assertRaises(BehaviorNotAllowedException):
-                func(context)
+        self.assertEqual(len(states), 2)
 
 
 class IStudyStateTestCase(unittest.TestCase):
-
     def test_study_state_create_study(self):
         state = IStudyState()
         with self.assertRaises(BehaviorNotAllowedException):
@@ -56,4 +52,3 @@ class IStudyStateTestCase(unittest.TestCase):
         state = IStudyState()
         with self.assertRaises(BehaviorNotAllowedException):
             state.change_state()
-
